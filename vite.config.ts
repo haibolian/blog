b2f9e67dc917dd4a81c297e08db40b02d7cd9279
lianhaibo <957377783@qq.com>
2022-05-13 22:45:20 +0800

    chore: add auto-import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
+import DefineOptions from 'unplugin-vue-define-options/vite';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue'
-import DefineOptions from 'unplugin-vue-define-options/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), DefineOptions()],
+  plugins: [
+    vue(), 
+    DefineOptions(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-import.d.ts"
+    })
+    
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname , 'src')
