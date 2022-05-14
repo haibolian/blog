bacda44d91f36ed183414915197047f7b1f1268b
lianhaibo <957377783@qq.com>
2022-05-14 21:54:14 +0800

    feat: base-info

---
+++ src/components/Avatar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="avatar">
+    <img
+      :src="src"
+      :alt="alt"
+      :height="height"
+      :width="width"
+    >
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'Avatar',
+})
+
+const props = defineProps({
+  src: String,
+  height: {
+    type: String,
+    default: '70'
+  },
+  width: {
+    type: String,
+    default: '70'
+  },
+  alt: String
+})
+
+
+</script>
+
+<style lang='scss' scoped>
+.avatar img{
+  border-radius: 50%;
+  box-shadow: 0 0 9px 2px #00000050;
+}
+</style>
--- src/layout/components/BaseInfo.vue
+++ src/layout/components/BaseInfo.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="base-info">
-    
+    <Avatar src="src/assets/images/avatar.png" width="70" height="70" alt="头像"></Avatar>
+    <div class="name">凌凡</div>
   </div>
 </template>
 
 <script setup>
-  defineOptions({
-    name: 'BaseInfo'
-  })
-  
+import Avatar from '@/components/Avatar.vue';
+defineOptions({
+  name: 'BaseInfo'
+})
+
+
 </script>
 
 <style lang='scss' scoped>
+.base-info {
+  box-sizing: border-box;
+  // text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
 
+}
+.name {
+  font-size: 20px;
+  font-family: fzls;
+}
+.mapbo {
+  position: absolute;
+}
 </style>
