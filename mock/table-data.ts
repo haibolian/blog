e62fb67a90b65a1803c863969c68f8f5b15166f8
lianhaibo <957377783@qq.com>
2022-06-09 15:01:24 +0800

    fix: test mock

---
+++ mock/table-data.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    url: "/getdata",
+    method: "get",
+    response: ({ query }) => {
+      return {
+        info: '1232',
+        success: true
+      }
+    }
+  }
+]
