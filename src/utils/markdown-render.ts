dfb5c41b0ef372557758f802a9124a9a969cc1c4
lianhaibo <957377783@qq.com>
2022-06-09 14:14:58 +0800

    fix: md add class

--- src/utils/markdown-render.ts
+++ src/utils/markdown-render.ts
@@ -4,9 +4,28 @@ import hljs from 'highlight.js';
 const md = new MarkdownIt({
   highlight: function (str, lang) {
     if (lang) return hljs.highlightAuto(str).value;
-    return '';
+    return "";
   }
+
 })
+
+md.core.ruler.push('show-line', (state)=> {
+  // forEach 遍历 tokens
+state.tokens.forEach(token => {
+        // token.map 即所在的行数
+        if (token.map) {
+          // 起始行
+            const start  = ["class", "l-md"];
+            // 结束行
+            // const end  = ["data-line-end", token.map[1].toString()]
+            // 初始化属性
+            token.attrs = token.attrs || []
+            // 添加属性
+            token.attrs.push(start)
+        }
+    })
+return true;
+});
 
 
 
